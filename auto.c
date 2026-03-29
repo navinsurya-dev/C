bfab67578608e1175a3a68966756ffb1264fefc4
navinsurya-dev <navinsuryacollegemail@gmail.com>
2026-03-29 13:54:09 +0530

    Created auto.c cstdv.c nullptr.c

---
+++ auto.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+    auto a = 5;
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0 ;
+}
---
+++ nullptr.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+//#define NULL ((void *) 0)
+
+int main(){
+
+    printf("%p\n",NULL);
+    printf("%p",nullptr);
+    return 0 ;
+}
